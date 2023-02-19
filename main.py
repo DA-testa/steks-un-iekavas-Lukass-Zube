310eb9cfd32fbbebee36524c8be07cdb51a2cbca
LukJr <75735232+LukJr@users.noreply.github.com>
2023-02-19 21:19:02 +0200

    Update

--- main.py
+++ main.py
@@ -16,19 +16,15 @@ def find_mismatch(text):
             opening_brackets_stack.append(Bracket(next,i))
             # Process opening bracket, write your code here
             
-
         if next in ")]}":
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
                 return i + 1
             opening_brackets_stack.pop()
             
-        
         if i == len(text) - 1:
             if opening_brackets_stack:
                 return opening_brackets_stack[0].position + 1
             return "Success"
-        
-            
 
 
 def main():
